86f6edddadf1fb7b63b092789cfc25e3ed7588bc
Rarstyak <rarstyak@gmail.com>
2024-04-03 16:31:24 -0400

    run rubocop, add validation template

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
+  validates :title,   presence: true, uniqueness: true
+  validates :body,    presence: true
+  validates :user_id, presence: true
+
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username,  presence: true, uniqueness: true, length: { mimimun: 4, maximum: 12 }
+  validates :email,     presence: true, uniqueness: true
+  validates :password,  presence: true, length: { mimimun: 6, maximum: 16 }
 
   has_many :posts
 end
